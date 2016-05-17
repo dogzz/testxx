1c38e15627191a811c46722cfae29544bba5c6d4
dogzz <afon@ukr.net>
2016-05-17 16:52:21 +0300

    Add Bloom Filter implementation

---
+++ src/com/company/kata05/bloomfilter/SpellCheck.java
@@ -0,0 +1,84 @@
+/*
+* Copyright Medtronic, Inc. 2014-2015
+*
+* MEDTRONIC CONFIDENTIAL - This document is the property of Medtronic,
+* Inc.,and must be accounted for. Information herein is confidential. Do
+* not reproduce it, reveal it to unauthorized persons, or send it outside
+* Medtronic without proper authorization.
+*/
+
+package com.company.kata05.bloomfilter;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.BitSet;
+
+public class SpellCheck {
+    private BitSet vector;
+    private BufferedReader reader;
+    private int linesCount;
+    public final static int vectorMultiplier = 3;
+    MessageDigest md;
+
+    public SpellCheck() {
+        try {
+            reader = new BufferedReader(new FileReader(new File("resources\\wordlist.txt")));
+            md = MessageDigest.getInstance("MD5");
+            linesCount = 400000; //(int) reader.lines().count();
+            vector = generateBitVector(reader);
+        } catch (FileNotFoundException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private BitSet generateBitVector(BufferedReader reader) {
+        vector = new BitSet(linesCount * vectorMultiplier);
+        reader.lines().forEach(s -> {
+            vector.set(getHash1(s), true);
+            vector.set(getHash2(s), true);
+            vector.set(getHash3(s), true);
+        });
+        reader.lines().close();
+        return vector;
+    }
+
+    public int getHash1(String word) {
+        return Math.abs(word.hashCode());
+    }
+
+    public int getHash2(String word) {
+        md.update(word.getBytes());
+        byte[] reducedHash = md.digest();
+        return new BigInteger(1, new byte[] {reducedHash[0], reducedHash[1]}).intValue();
+    }
+
+    public int getHash3(String word) {
+        md.update(word.getBytes());
+        byte[] reducedHash = md.digest();
+        return new BigInteger(1, new byte[] {reducedHash[2], reducedHash[3]}).intValue();
+    }
+
+    public BitSet getVector() {
+        return vector;
+    }
+
+    public BufferedReader getReader() {
+        return reader;
+    }
+
+
+    public int getLinesCount() {
+        return linesCount;
+    }
+
+    public boolean isWordCorrect(String word) {
+        return vector.get(getHash1(word))
+                && vector.get(getHash2(word))
+                && vector.get(getHash3(word));
+    }
+}
---
+++ test/com/company/kata05/bloomfilter/SpellCheckTest.java
@@ -0,0 +1,89 @@
+package com.company.kata05.bloomfilter;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/*
+* Copyright Medtronic, Inc. 2014-2015
+*
+* MEDTRONIC CONFIDENTIAL - This document is the property of Medtronic,
+* Inc.,and must be accounted for. Information herein is confidential. Do
+* not reproduce it, reveal it to unauthorized persons, or send it outside
+* Medtronic without proper authorization.
+*/
+public class SpellCheckTest {
+    SpellCheck spellCheck;
+    List<String> correctWords;
+    List<String> incorrectWords;
+    BufferedReader reader;
+
+    @Before
+    public void setUp() throws Exception {
+        spellCheck = new SpellCheck();
+        reader = new BufferedReader(new FileReader(new File("resources\\wordlist.txt")));
+        correctWords = new ArrayList<>();
+        incorrectWords = new ArrayList<>();
+        correctWords.add(reader.lines().findAny().orElse("preambulate"));
+        correctWords.add(reader.lines().findAny().orElse("loudliest"));
+        correctWords.add(reader.lines().findAny().orElse("discipline"));
+        correctWords.add(reader.lines().findAny().orElse("teemingly"));
+        incorrectWords.add("asdfg");
+        incorrectWords.add("teeminglywe");
+        incorrectWords.add("qwtr");
+        incorrectWords.add("hjk");
+    }
+
+    @Test
+    public void testVectorNotNull() throws Exception {
+        Assert.assertNotNull(spellCheck.getVector());
+    }
+
+    @Test
+    public void testVectorSize() throws Exception {
+        Assert.assertTrue(spellCheck.getVector().size() != 0);
+    }
+
+    @Test
+    public void testReaderNotNull() throws Exception {
+        Assert.assertNotNull(spellCheck.getReader());
+    }
+
+    @Test
+    public void testCorrectWords() throws Exception {
+        for (String s : correctWords) {
+            Assert.assertTrue("Verify that word " + s + " is correct", spellCheck.isWordCorrect(s));
+        }
+    }
+
+    @Test
+    public void testIncorrectWords() throws Exception {
+        for (String s : incorrectWords) {
+            Assert.assertFalse("Verify that word " + s + " is incorrect", spellCheck.isWordCorrect(s));
+        }
+    }
+
+    @Test
+    public void testEfficiencyOfBloomFilter() throws Exception  {
+        long startTime = new Date().getTime();
+        Assert.assertTrue(spellCheck.isWordCorrect("teemingly"));
+        long endTime = new Date().getTime();
+        long bloomResult = endTime - startTime;
+        reader = new BufferedReader(new FileReader(new File("resources\\wordlist.txt")));
+        startTime = new Date().getTime();
+        Assert.assertTrue(reader.lines().anyMatch(s -> {
+            return s.equals("teemingly");
+        }));
+        endTime = new Date().getTime();
+        long iterativeResult = endTime - startTime;
+        System.out.println("The Bloom result is " + bloomResult + "; The itertive result is " + iterativeResult);
+        Assert.assertTrue(bloomResult < iterativeResult);
+    }
+}
